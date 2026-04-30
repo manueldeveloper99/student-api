4a4c987ca7dc64ec2fc2a068cdc054ab06c1449f
manueldeveloper99 <manueldeveloper99cs04@gmail.com>
2026-04-29 18:30:33 -0600

    Añadiendo archivo

--- src/main/java/com/example/student_api/StudentApiApplication.java
+++ src/main/java/com/example/student_api/StudentApiApplication.java
@@ -1,13 +1,14 @@
 package com.example.student_api;
+
 //prueba repositorio 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // Class  as source definations beans
+@SpringBootApplication // Class as source definations beans
 public class StudentApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApiApplication.class, args);
 	}
-
+	// changes
 }
